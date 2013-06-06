b0977d5aa99c981b8f58bcffd49c022e13fe5fbe
hrmj007@hotmail.com <hrmj007@hotmail.com>
2013-06-05 23:57:24 -0400

    first push master

---
+++ JavaApplication1/src/javaapplication1/NewClass.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author Chris
+ */
+public class NewClass {
+    String a_2;
+}
